8f88b467b3b621923b7a349faf8c87eb4683b88c
Dadore76 <80016500+Dadore76@users.noreply.github.com>
2023-10-12 18:06:17 -0500

    Adding files to ignore and more fields to csv

--- facturapdf/creacion_manual.py
+++ facturapdf/creacion_manual.py
@@ -10,7 +10,10 @@ def generar_factura():
                      apmt=record['apmt'],
                      value=record['value'],
                      type=record['type'],
-                     month_name=record['month_name'])
+                     month_name=record['month_name'],
+                     savings=record['saving'],
+                     penalty=record['penalty'],
+                    )
         
 def leer_archivo():
     df = pandas.read_csv('Data/generar.csv')
